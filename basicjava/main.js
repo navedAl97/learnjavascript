4421f0d6c0bd00ab2f6e552f860b0c4275aa03c3
Naved Alam <161110585+navedAl97@users.noreply.github.com>
2025-02-16 10:46:05 +0000

    varible

--- basicjava/main.js
+++ basicjava/main.js
@@ -1,1 +1,8 @@
-console.log("naved alam")
+const accountId = 665
+var accountName = "Naved Alam"
+let accountEmail = "navedalam278gmail.com"
+let UserState;
+
+
+// console.log(accountEmail);
+console.table(["Id", accountId, "Name", accountName, "Email", accountEmail, "state", UserState])
